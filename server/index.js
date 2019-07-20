10a18adb87ad6aa3806635e2f45e869843d10933
ch4admin <wahlswede@accel-kkr.com>
2019-07-20 08:31:58 -0700

    node server

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ if (!isDev && cluster.isMaster) {
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../ui/public', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../public', 'index.html'));
   });
 
   app.listen(PORT, function () {
